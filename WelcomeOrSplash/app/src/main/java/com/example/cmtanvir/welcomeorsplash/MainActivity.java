7063315faa07cb886a50e76fe0a37e19da52da7d
Imam Hossain <imam2889@gmail.com>
2018-02-06 02:48:59 +0600

    This is Welcome Screen or Splash Screen

---
+++ WelcomeOrSplash/app/src/main/java/com/example/cmtanvir/welcomeorsplash/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cmtanvir.welcomeorsplash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Remove Title Bar
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
