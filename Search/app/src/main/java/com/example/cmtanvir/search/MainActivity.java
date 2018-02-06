0ac9eb158f462cf923385893025f2be92a1e4874
Imam Hossain <imam2889@gmail.com>
2018-02-06 23:06:02 +0600

    How to add Search Option into Action Bar

---
+++ Search/app/src/main/java/com/example/cmtanvir/search/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cmtanvir.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView= (ListView) findViewById(R.id.listId);
+        String[] countryNames = getResources().getStringArray(R.array.country_names);
+
+
+
+    }
+}
