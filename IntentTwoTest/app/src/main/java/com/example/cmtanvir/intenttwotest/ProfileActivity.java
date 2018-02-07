7e5e5f9333247a0f102a1eb519484bd2495d8de4
Imam Hossain <imam2889@gmail.com>
2018-02-07 12:23:48 +0600

    How to Jump The Next Activity using Intent

---
+++ IntentTwoTest/app/src/main/java/com/example/cmtanvir/intenttwotest/ProfileActivity.java
@@ -0,0 +1,24 @@
+package com.example.cmtanvir.intenttwotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        textView =(TextView) findViewById(R.id.textId);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle!=null){
+           String value = bundle.getString("tag");
+           textView.setText(value);
+        }
+    }
+}
